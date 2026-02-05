d014dc2cb123cfeb7228b36c880ec7fa19346e3e
Kitorosano <rosanoesteban2@gmail.com>
2026-02-05 10:20:50 -0300

    feat: :sparkles: Se agrega logica para filtrado de libros mediante campo de busqueda

--- js/books.js
+++ js/books.js
@@ -18,7 +18,7 @@ let books = [
   },
 ];
 
-// Logica
+// ======== Logica para mostrar la lista de libros ========
 const $LIST = document.getElementById("books-list");
 
 // por cada libro de mi lista
@@ -30,4 +30,31 @@ books.forEach(function (book, i) {
 
   // agregar el <li> a la lista
   $LIST.appendChild($ITEM);
+});
+
+// ======== Logica para filtrar libros por titulo ========
+const $SEARCH_INPUT = document.getElementsByName("q")[0];
+
+// Agregar un evento de input al campo de búsqueda
+$SEARCH_INPUT.addEventListener("input", function () {
+  // obtener el valor del input de búsqueda
+  const searchTitle = $SEARCH_INPUT.value;
+
+  // filtrar los libros por título utilizando el método filter y
+  // comparando el título del libro con el valor de búsqueda (ignorando mayúsculas y minúsculas)
+  const filterBooks = books.filter(function (book) {
+    const bookTitleLowercase = book.title.toLowerCase();
+    const searchTitleLowercase = searchTitle.toLowerCase();
+    return bookTitleLowercase.includes(searchTitleLowercase);
+  });
+
+  // Limpiar la lista antes de mostrar los resultados
+  $LIST.innerHTML = "";
+
+  // Volver a generar HTML para mostrar <li> por cada libro filtrado
+  filterBooks.forEach(function (book) {
+    let $ITEM = document.createElement("li");
+    $ITEM.innerHTML = `${book.id}. "${book.title}" de ${book.author}`;
+    $LIST.appendChild($ITEM);
+  });
 });
